36f62662d21c9598b0a73ae47122d114189ce662
Andrey Dorofeev <adorofeev@icerockdev.com>
2022-09-09 12:28:03 +0700

    Initial Commit

---
+++ RandomSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomSchedule
+//
+//  Created by Andrey Dorofeev on 09.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
